3842da245251e1016a140d32e534a4ab88a977c2
Mark Needham <m.h.needham@gmail.com>
2019-12-19 15:27:48 +0000

    dummy react app

---
+++ blog/content/2019/12/my-app/src/App.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import {Link, Router} from "@reach/router";
+import {globalHistory} from '@reach/router';
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
+
+const Search = () => (
+  <div>
+    <h2>Search</h2>
+  </div>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showSearchBox: true
+    };
+  }
+
+  componentDidMount() {
+    this.toggleSearchBox(globalHistory.location)
+
+    this.historyUnsubscribe = globalHistory.listen(({action, location}) => {
+      if (action === 'PUSH') {
+        this.toggleSearchBox(location)
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.historyUnsubscribe();
+  }
+
+  toggleSearchBox(location) {
+    if (location.pathname === "/search") {
+      this.setState({
+        showSearchBox: false
+      })
+    } else {
+      this.setState({
+        showSearchBox: true
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            React Reach Router Demo
+          </p>
+          <div>
+            {this.state.showSearchBox && <input type={"text"} style={{margin: "10px"}}/>}
+          </div>
+        </header>
+        <nav style={{padding: "5px"}}>
+          <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link> <Link to="search">Search</Link>
+        </nav>
+        <div style={{paddingTop: "10px"}}>
+          <Router>
+            <Home path="/"/>
+            <Dashboard path="/dashboard"/>
+            <Search path="/search"/>
+          </Router>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
