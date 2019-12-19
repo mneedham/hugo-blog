7a57db22455f52e7f715358d5cd07893b6c20249
Mark Needham <m.h.needham@gmail.com>
2019-12-19 16:30:00 +0000

    updates

--- blog/content/2019/12/my-app/src/App.js
+++ blog/content/2019/12/my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import './App.css';
 import {globalHistory, Link, Router} from "@reach/router";
 
@@ -21,64 +21,104 @@ const Search = () => (
   </div>
 );
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showSearchBox: true
-    };
-  }
 
-  componentDidMount() {
-    this.toggleSearchBox(globalHistory.location)
+//
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       showSearchBox: true
+//     };
+//   }
+//
+//   componentDidMount() {
+//     this.toggleSearchBox(globalHistory.location)
+//
+//     this.historyUnsubscribe = globalHistory.listen(({action, location}) => {
+//       if (action === 'PUSH') {
+//         this.toggleSearchBox(location)
+//       }
+//     });
+//   }
+//
+//   componentWillUnmount() {
+//     this.historyUnsubscribe();
+//   }
+//
+//   toggleSearchBox(location) {
+//     if (location.pathname === "/search") {
+//       this.setState({
+//         showSearchBox: false
+//       })
+//     } else {
+//       this.setState({
+//         showSearchBox: true
+//       })
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <p>
+//             React Reach Router Demo
+//           </p>
+//           <div>
+//             {this.state.showSearchBox && <input type={"text"} style={{margin: "10px"}}/>}
+//           </div>
+//         </header>
+//         <nav style={{padding: "5px"}}>
+//           <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link> <Link to="search">Search</Link>
+//         </nav>
+//         <div style={{paddingTop: "10px"}}>
+//           <Router>
+//             <Home path="/"/>
+//             <Dashboard path="/dashboard"/>
+//             <Search path="/search"/>
+//           </Router>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-    this.historyUnsubscribe = globalHistory.listen(({action, location}) => {
-      if (action === 'PUSH') {
-        this.toggleSearchBox(location)
-      }
+function App() {
+  const initialState = true
+
+  const [showSearchBox, setShowSearchBox]= useState(initialState)
+  useEffect(() => {
+    const removeListener = globalHistory.listen(params => {
+      const { location } = params;
+      const newState = location.pathname !== "/search"
+      setShowSearchBox(newState);
     });
-  }
+    return () => {
+      removeListener();
+    };
+  }, []);
 
-  componentWillUnmount() {
-    this.historyUnsubscribe();
-  }
-
-  toggleSearchBox(location) {
-    if (location.pathname === "/search") {
-      this.setState({
-        showSearchBox: false
-      })
-    } else {
-      this.setState({
-        showSearchBox: true
-      })
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            React Reach Router Demo
-          </p>
-          <div>
-            {this.state.showSearchBox && <input type={"text"} style={{margin: "10px"}}/>}
-          </div>
-        </header>
-        <nav style={{padding: "5px"}}>
-          <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link> <Link to="search">Search</Link>
-        </nav>
-        <div style={{paddingTop: "10px"}}>
-          <Router>
-            <Home path="/"/>
-            <Dashboard path="/dashboard"/>
-            <Search path="/search"/>
-          </Router>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Reach Router Demo</p>
+        <div>
+          {showSearchBox && <input type={"text"} style={{ margin: "10px" }} />}
         </div>
+      </header>
+      <nav style={{ padding: "5px" }}>
+        <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>{" "}
+        <Link to="search">Search</Link>
+      </nav>
+      <div style={{ paddingTop: "10px" }}>
+        <Router>
+          <Home path="/" />
+          <Dashboard path="/dashboard" />
+          <Search path="/search" />
+        </Router>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
